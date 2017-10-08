7b36b26f3fb3f23be2fd7eaad086f09bd467fab0
Jesus Larrubia <jesus.larrubia@siftmedia.co.uk>
2017-10-08 16:55:09 +0100

    Improved version - applied security patch.

--- theme/js/nodeviewcount.js
+++ theme/js/nodeviewcount.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function($) {
   var nodeviewcount_insert_node_view = function(nid, uid) {
     var nodeviewcount_path = Drupal.settings.nodeviewcount.nodeviewcount_path;
+    var nodeviewcount_timestamp = Drupal.settings.nodeviewcount.nodeviewcount_timestamp;
+    var nodeviewcount_token = Drupal.settings.nodeviewcount.nodeviewcount_token;
     $.ajax({
       type: 'POST',
-      url: Drupal.settings.basePath + nodeviewcount_path + '/' + nid + '/' + uid,
+      url: Drupal.settings.basePath + nodeviewcount_path + '/' + nid + '/' + uid + '/' + nodeviewcount_timestamp + '/' + nodeviewcount_token,
       dataType: 'json'
     });
   }
